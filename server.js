f993911c6d3b2e4210682945f2badcbba379c934
Rosmary Fermin <Rosmaryfc@hotmail.com>
2020-08-06 21:32:10 -0400

    fix deployment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
 const routes = require('./routes/index.js');
-// const db = require('./db/index')
+const db = require('./db/index')
 
 
 //GLOBAL VARIABLES
